92e41b84f4fa3a9ec222ed9d97e313ebffee8dbf
codekid69 <bishtyash069@gmail.com>
2025-08-31 11:52:53 +0530

    fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,6 @@ const links = [
   { label: "Testimonial", to: "/testimonial" },
   { label: "FAQ", to: "/faq" },
   { label: "CSR", to: "/csr" },
-  { label: "Blogs", to: "/blogs" },
   { label: "Explore", to: "/explore" },
 ];
 
