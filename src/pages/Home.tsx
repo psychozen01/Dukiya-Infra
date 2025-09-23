5afcc6232336b209cd4da21a6fa47aa5b7aa9dc5
codekid69 <bishtyash069@gmail.com>
2025-09-23 16:29:16 +0530

    added pics

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,10 @@
 import heroImg from "@/assets/home/hero.svg";
 import img1 from "@/assets/home/img1.svg";     // skyscraper image
 import img2 from "@/assets/home/img2.svg";   // re-imagining horizon image
-import i1 from "@/assets/home/img3.svg"; // feature (tall)
-import i2 from "@/assets/home/img4.svg"; // small
-import i3 from "@/assets/home/img5.svg"; // small
-import i4 from "@/assets/home/img6.svg"; // wide
+import i1 from "@/assets/home/img3.png"; // feature (tall)
+import i2 from "@/assets/home/img4.png"; // small
+import i3 from "@/assets/home/img5.png"; // small
+import i4 from "@/assets/home/img6.png"; // wide
 import { useEffect} from "react";
 import {  X,} from "lucide-react";
 import { Calendar, BookOpen, ChevronLeft, ChevronRight, ArrowRight } from "lucide-react";
